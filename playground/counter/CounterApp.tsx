bcdaf6428f84f0c7a6b19c404bcc340ebf7d0c43
Deberson Paula <deberson.paula@gmail.com>
2020-08-22 14:58:05 -0300

    added counter

--- playground/counter/CounterApp.tsx
+++ playground/counter/CounterApp.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Store } from 'reactoom';
+import { useModelContext } from 'reactoom';
 import { CounterModel } from './CounterModel';
 import { CounterComponent } from './CounterComponent';
 
 export function CounterApp(): JSX.Element {
-  return (
-    <Store models={[CounterModel]}>
-      <CounterComponent />
-    </Store>
-  );
+  const counterContext = useModelContext(CounterModel);
+  return <CounterComponent count={counterContext.count} add={counterContext.add} del={counterContext.del} />;
 }
--- playground/counter/CounterComponent.tsx
+++ playground/counter/CounterComponent.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
-export function CounterComponent(): JSX.Element {
-  return <div>Counter</div>;
+export function CounterComponent(props: IProps): JSX.Element {
+  return (
+    <div>
+      <p>Counter = {props.count}</p>
+      <p>
+        <button onClick={props.add}>Add</button>
+        <button onClick={props.del}>Del</button>
+      </p>
+    </div>
+  );
+}
+
+interface IProps {
+  count: number;
+  add: () => void;
+  del: () => void;
 }
